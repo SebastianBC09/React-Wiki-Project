6dca731febbdf5cdad550720044d38cfa1301cff
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-05-01 18:43:21 -0500

    List view file edited: container created, tailwind classes added & basic layout finished.

--- src/components/RickAndMorty/List/List.jsx
+++ src/components/RickAndMorty/List/List.jsx
@@ -12,7 +12,16 @@ const RickAndMortyList = ({ characters, getAllCharacters }) => {
 
   console.log(characters);
   return (
-    <section>
+    <section className="container mx-auto grid grid-cols-3 gap-5">
+      <section className="bg-green-300">Morty</section>
+      <section className="bg-blue-300">MrPoppybutthole</section>
+      <section className="bg-indigo-300">BirdMan</section>
+      <section className="bg-purple-300">BirdMan</section>
+      <section className="bg-pink-300">BirdMan</section>
+      <section className="bg-yellow-300">BirdMan</section>
+      <section className="bg-red-300">BirdMan</section>
+      <section className="bg-gray-300">BirdMan</section>
+      <section className="bg-green-400">BirdMan</section>
     </section>
   )
 };
