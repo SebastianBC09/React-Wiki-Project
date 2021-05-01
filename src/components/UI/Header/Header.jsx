c498ee655eef520dc99386435fb24bc796ceebc6
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-04-30 21:35:26 -0500

    Header CSS filed edited: Tailwind classed added. Styling, colours, sizes & more added to it.

--- src/components/UI/Header/Header.jsx
+++ src/components/UI/Header/Header.jsx
@@ -3,7 +3,14 @@ import './Header.css';
 
 const Header = () => (
   <header>
-    <h3>React Wiki</h3>
+    <section className="container mx-auto flex items-center justify-between h-full">
+      <h3>React Wiki</h3>
+      <select className="api_select">
+        <option value="Rick_Morty">Rick & Morty API</option>
+        <option value="Futurama">Futurama API</option>
+        <option value="Pokemon">Pokemon API</option>
+      </select>
+    </section>
   </header>
 );
 
